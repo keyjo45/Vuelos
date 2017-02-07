8da2d3b333a17d753eaa11d6e55f5a458027601d
Yosimar Valega <yosimar.valega@techandsolve.com>
2017-02-07 11:12:47 -0500

    Arreglo de pruebas

--- VuelosWeb/src/test/java/co/com/techandsolve/aerotech/services/UsuarioServiceTest.java
+++ VuelosWeb/src/test/java/co/com/techandsolve/aerotech/services/UsuarioServiceTest.java
@@ -12,6 +12,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import co.com.techandsolve.aerotech.beans.SecurityBean;
 import co.com.techandsolve.aerotech.daos.UsuarioDao;
 import co.com.techandsolve.aerotech.exception.ValidacionException;
 import co.com.techandsolve.aerotech.models.Usuario;
@@ -22,29 +23,52 @@ public class UsuarioServiceTest {
 
 	@InjectMocks
 	private UsuarioServices usuarioServices;
-	
+
 	@Mock
 	UsuarioDao usuarioDao;
-	
-	
+
+	@Mock
+	SecurityBean securityBean;
+
+	@Mock
+	Usuario usuarioObtenido;
+
 	@Test
-	public void debeAdicionarCliente() throws ValidacionException{
-		
+	public void debeAdicionarCliente() throws ValidacionException {
+
 		Usuario usuario = new Usuario();
 		usuario.setPassword(Utilidades.generarCodificacion("1234"));
 		List<Usuario> listaUsuario = new ArrayList<>();
 		Mockito.when(usuarioDao.consultarUsuario(usuario)).thenReturn(listaUsuario);
 		usuarioServices.addClient(usuario);
-		Mockito.verify(usuarioDao).crearUsuario(usuario);	
+		Mockito.verify(usuarioDao).crearUsuario(usuario);
 	}
-	
-	@Test(expected=ValidacionException.class)
-	public void noDebeAdicionarCliente() throws ValidacionException{
-		
+
+	@Test(expected = ValidacionException.class)
+	public void noDebeAdicionarCliente() throws ValidacionException {
+
 		Usuario usuario = new Usuario();
 		usuario.setPassword(Utilidades.generarCodificacion("1234"));
 		List<Usuario> listaUsuario = Arrays.asList(new Usuario());
 		Mockito.when(usuarioDao.consultarUsuario(usuario)).thenReturn(listaUsuario);
 		usuarioServices.addClient(usuario);
+	}
+
+	@Test
+	public void debeRetornarUsuario() throws ValidacionException {
+
+		Usuario usuario = new Usuario();
+		String passwordEncript = Utilidades.generarCodificacion("123");
+		Mockito.when(securityBean.login("Yosimar", passwordEncript)).thenReturn(usuario);
+		usuarioObtenido = usuarioServices.login("Yosimar", "123");
+		Assert.assertEquals(usuario, usuarioObtenido);
+	}
+
+	@Test(expected = ValidacionException.class)
+	public void noDebeRetornarUsuario() throws ValidacionException {
+
+		String passwordEncript = Utilidades.generarCodificacion("123");
+		Mockito.when(securityBean.login("Yosimar", passwordEncript)).thenThrow(ValidacionException.class);
+		usuarioObtenido = usuarioServices.login("Yosimar", "123");
 	}
 }
